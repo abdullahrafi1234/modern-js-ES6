4783e58f3a2bcd38ea51cf01c9fe3b59bee015d1
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-10-01 13:06:05 +0600

    practice js with sumit

--- thisKeyword.js
+++ thisKeyword.js
@@ -1,3 +1,15 @@
-// Javascript this keyword
+// Javascript this keyword ----> 
+
+// implicit binding
+// explicit binding
+// new binding
+// window binding
 
 
+
+var printPlayerName = function(name){
+	console.log(name)
+}
+
+
+printPlayerName('Shakib')
