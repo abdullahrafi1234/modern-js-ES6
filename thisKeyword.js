5cec3eb4748b8ed9e7136e6b8e173d0fb273f147
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2024-10-02 12:42:54 +0600

    practice js with sumit

--- thisKeyword.js
+++ thisKeyword.js
@@ -1,7 +1,7 @@
 //JavaScript 'this' keyword and - .call(), .apply() and .bind()  --->
 
-// implicit binding
-// explicit binding
+// implicit binding   (dot notation diye korte hoy)
+// explicit binding   (nije theke bole deoa hoy)
 // new binding
 // window binding
 
